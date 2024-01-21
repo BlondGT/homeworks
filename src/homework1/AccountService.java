2e5d6f6420ca34016c36ce8f3933df9880702693
BlondGT <112488079+BlondGT@users.noreply.github.com>
2024-01-21 10:19:01 -0500

    Fixed class AccountService

--- src/homework1/AccountService.java
+++ src/homework1/AccountService.java
@@ -1,38 +1,43 @@
 import java.time.Month;
-import java.util.*;
+import java.time.Year;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class AccountService {
 
-    public static List<Account> over500Balance (List<Account> accounts){
+    public List<Account> over500Balance (List<Account> accounts){
         return accounts.stream()
                 .filter(account -> account.getBalance() >= 500)
                 .toList();
     }
-    public static Set<String> listCountries(List<Account> accounts) {
+    public Set<String> listCountries(List<Account> accounts) {
         return accounts.stream()
                 .map(Account::getCountry)
                 .collect(Collectors.toSet());
     }
 
-    public static boolean youngerThan2000Year(List<Account> accounts){
+    public boolean youngerThan2000Year(List<Account> accounts){
         return accounts.stream()
                 .allMatch(account -> account.getBirthday().getYear() < 2000);
     }
 
-    public static double sumAllMaleBalance(List<Account> accounts){
+    public double sumAllMaleBalance(List<Account> accounts){
         return accounts.stream()
                 .filter(account -> account.getGender().equals("Male"))
                 .map(Account::getBalance)
                 .reduce( 0.0, Double::sum);
     }
 
-    public static Map<Month, List<Account>> groupingByMonthBirth(List<Account> accounts){
+    public Map<Month, List<Account>> groupingByMonthBirth(List<Account> accounts){
         return accounts.stream()
                 .collect(Collectors.groupingBy(account -> account.getBirthday().getMonth()));
     }
 
-    public static double averageBalanceByCountry(List<Account> accounts, String country){
+    public double averageBalanceByCountry(List<Account> accounts, String country){
         return accounts.stream()
                 .filter(account -> account.getCountry().equals(country))
                 .mapToDouble(Account::getBalance)
@@ -40,30 +45,31 @@ public class AccountService {
                 .orElse(0);
     }
 
-    public static List<String> listFirstNameCommaLastName(List<List<Account>> accountsLists){
+    public List<String> listFirstNameCommaLastName(List<List<Account>> accountsLists){
         return accountsLists.stream()
                 .flatMap(List::stream)
                 .map(account -> account.getFirstName() + ", " + account.getLastName())
                 .collect(Collectors.toList());
     }
 
-    public static List<Account> sortingByLastNameAndFirstName(List<Account> accounts){
+    public List<Account> sortingByLastNameAndFirstName(List<Account> accounts){
         return accounts.stream()
                 .sorted(Comparator.comparing(Account::getLastName).thenComparing(Account::getFirstName))
                 .collect(Collectors.toList());
     }
 
-    public static Optional<Account> oldestAccount(List<Account> accounts){
+    public Optional<Account> oldestAccount(List<Account> accounts){
         return accounts.stream()
                 .max(Comparator.comparing(Account::getBirthday));
     }
 
-    public static Map<Double, Long> sortingByPlaceAndBalance(List<Account> accounts){
+    public Map<Year, Double> sortingByPlaceAndBalance(List<Account> accounts){
         return accounts.stream()
-                .collect(Collectors.groupingBy(Account::getBalance, Collectors.counting()));
+                .collect(Collectors.groupingBy(account -> Year.of(account.getBirthday().getYear()),
+                        Collectors.averagingDouble(Account::getBalance)));
     }
 
-    public static Optional<Account> longestLastName(List<Account> accounts){
+    public Optional<Account> longestLastName(List<Account> accounts){
         return  accounts.stream()
                 .max(Comparator.comparingInt(account -> account.getLastName().length()));
     }
